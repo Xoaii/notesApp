ab98679fdb78e53d0eb7b3140bb6fb1bfd1b0c6e
Xoaii <phamtuanduongtn2013@gmail.com>
2024-02-28 23:20:28 +0700

    đang làm giở save note

---
+++ app/src/main/java/com/example/notesapp/NoteDetailsActivity.java
@@ -0,0 +1,30 @@
+package com.example.notesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class NoteDetailsActivity extends AppCompatActivity {
+    EditText titleEditText,contentEditText;
+    ImageButton saveNoteBtn
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_details);
+        titleEdit=findViewById(R.id.notes_title_text);
+        contentEdit=findViewById(R.id.notes_content_text);
+        saveNoteBtn=findViewById(R.id.save_note_btn);
+        saveNoteBtn.setOnClickListener((v)->saveNote());
+
+    }
+    void saveNote(){
+        String noteTitle = titleEditText.getText().toString();
+        String noteContent=contentEditText.getText().toString();
+        if(noteTitle==null|| noteTitle.isEmpty()){
+            titleEditText.setError("Title required");
+        }
+    }
+}
